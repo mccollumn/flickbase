d2f1e77a40aaceb0c7d956e0ba2b624edfe61e75
Nick McCollum <nmccollum81@gmail.com>
2025-04-29 14:04:09 -0700

    Implement TipTap editor with formatting options and dynamic menus

--- frontend/src/utils/forms/tiptap.tsx
+++ frontend/src/utils/forms/tiptap.tsx
@@ -7,8 +7,89 @@ import {
 import StarterKit from "@tiptap/starter-kit";
 import "../../styles/tiptap.scss";
 
-const TipTap = ({ setEditorState, editorContent = "" }) => {
-  return <></>;
+interface TipTapProps {
+  setEditorState: (content: string) => void;
+  editorContent?: string;
+}
+
+const TipTap = ({ setEditorState, editorContent = "" }: TipTapProps) => {
+  const editor = useEditor({
+    content: editorContent,
+    extensions: [StarterKit],
+    onUpdate: ({ editor }) => {
+      setEditorState(editor.getHTML());
+    },
+  });
+  return (
+    <>
+      <div>
+        {editor && (
+          <BubbleMenu
+            className="bubble-menu"
+            tippyOptions={{ duration: 100 }}
+            editor={editor}
+          >
+            <button
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className={editor.isActive("bold") ? "is-active" : ""}
+            >
+              Bold
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              className={editor.isActive("italic") ? "is-active" : ""}
+            >
+              Italic
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleStrike().run()}
+              className={editor.isActive("strike") ? "is-active" : ""}
+            >
+              Strike
+            </button>
+          </BubbleMenu>
+        )}
+
+        {editor && (
+          <FloatingMenu
+            className="floating-menu"
+            tippyOptions={{ duration: 100 }}
+            editor={editor}
+          >
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 1 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 1 }) ? "is-active" : ""
+              }
+            >
+              H1
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 2 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 2 }) ? "is-active" : ""
+              }
+            >
+              H2
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleBulletList().run()}
+              className={editor.isActive("bulletList") ? "is-active" : ""}
+            >
+              Bullet List
+            </button>
+          </FloatingMenu>
+        )}
+      </div>
+      <span>
+        <EditorContent editor={editor} />
+      </span>
+    </>
+  );
 };
 
 export default TipTap;
